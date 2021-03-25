292c55f649a111be653d1fc1a3614daf8831012f
sam bacha <sam@freighttrust.com>
2021-03-25 14:09:18 -0700

    Update log.ts

--- source/models/log.ts
+++ source/models/log.ts
@@ -1,9 +1,9 @@
 // @file Log
 // @exports LogInterface
 export interface Log {
-    readonly blockNumber: number;
-    readonly blockHash: string;
-    readonly transactionIndex: number;
-    readonly logIndex: string;
-	readonly topics: string[],
+	readonly blockNumber: string;
+	readonly blockHash: string;
+	readonly transactionIndex: number;
+	readonly logIndex: string;
+	readonly topics: string[];
 }
