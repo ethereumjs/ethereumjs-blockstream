fac7b8c431215396b8d1b0eb43d3d7334817c55a
sam bacha <sam@freighttrust.com>
2021-03-25 13:58:17 -0700

    Update block.ts

--- source/models/block.ts
+++ source/models/block.ts
@@ -1,7 +1,12 @@
+// @file Block
+// @exports Block
+import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
 import { Transaction } from "./transaction";
 
 export interface Block {
-	readonly number: string;
+	readonly number: number;
 	readonly hash: string;
 	readonly parentHash: string;
+	readonly difficulty: number;
+        readonly gasLimit: BigNumber;
 }
