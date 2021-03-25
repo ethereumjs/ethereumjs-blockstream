5cb47fbee115ac7175dcc096a63e2740a2cf61df
sam bacha <sam@freighttrust.com>
2021-03-25 14:07:17 -0700

    Update block.ts

--- source/models/block.ts
+++ source/models/block.ts
@@ -4,7 +4,7 @@ import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
 import { Transaction } from "./transaction";
 
 export interface Block {
-	readonly number: number;
+	readonly number: string; //(parseHexInt(parentBlock.number) + blockRetention < parseHexInt(blockHistory.last().number))
 	readonly hash: string;
 	readonly parentHash: string;
 	readonly difficulty: number;
